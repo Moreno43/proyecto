ff227590fd11a3cfee10f1983f07e35fe56ceb27
juanp <juanpamolopez@outlook.com>
2023-03-17 02:17:36 -0500

    proyecto

---
+++ proyecto/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>Carrito</h1>
+    {{ productos }}
+    <hr>
+    <div class="row">
+      <Card v-for="producto of productos" :key="producto.id" :producto="producto" />
+    </div>
+  </div>
+</template>
+
+<script>
+import {useStore} from 'vuex'
+import { computed, onMounted } from 'vue'
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  setup() {
+    const store = useStore()
+    onMounted(() => {
+      store.dispatch('fetchData')
+    })
+
+    const productos = computed(() => store.state.productos)
+
+    return {productos}
+
+  }
+}
+</script>
