3fb766c03eed82aa4b5f434c04561652c7270433
juanp <juanpamolopez@outlook.com>
2023-03-23 12:37:01 -0500

    actualizacion

--- proyecto/src/components/Carrito.vue
+++ proyecto/src/components/Carrito.vue
@@ -1,52 +1,46 @@
-import { useStore } from 'vuex';
 <template>
   <div class="my-5">
-          <h4>Carrito de compras</h4>
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Item</th>
-                <th scope="col">Cantidad</th>
-                <th scope="col">Acción</th>
-                <th scope="col">Total</th>
-              </tr>
-            </thead>
-            <tbody id="items">
-              {{ item }}
-              <Item v-for="item in items" :key="item.id"
-              :item="item" />
-            </tbody>
-            <tfoot>
-              <tr id="footer">
-                <th scope="row" colspan="5" v-if="Object.keys(items).length === 0">Carrito vacío - comience a comprar!</th>
-                <Footer v-else />
-              </tr>
-            </tfoot>
-          </table>
-          <h4>Cards</h4>
-          <div class="row" id="cards"></div>
-        </div>
+    <h4>Carrito de compras</h4>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Item</th>
+          <th scope="col">Cantidad</th>
+          <th scope="col">Acción</th>
+          <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody id="items">
+        {{ item }}
+        <Item v-for="item in items" :key="item.id" :item="item" />
+      </tbody>
+      <tfoot>
+        <tr id="footer">
+          <th scope="row" colspan="5" v-if="Object.keys(items).length === 0">Carrito vacío - comience a comprar!</th>
+          <Footer v-else />
+        </tr>
+      </tfoot>
+    </table>
+  </div>
 </template>
 
 <script>
-import { Computed } from 'vuex'
-import {useStore} from 'vuex'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 import Item from './Item.vue'
 import Footer from './Footer.vue'
 
 export default {
-  components: {Item},
+  components: { Item, Footer },
 
   setup() {
     const store = useStore()
     const items = computed(() => store.state.carrito)
 
-    return {items}
-  } 
+    return { items }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
